dc73c2ab4f972a3014e46a55a313c72eb386d4fa
EduarnoTech <tech.eduarno@gmail.com>
2022-06-17 17:06:44 +0530

    sdfdfdf

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
+const path =require('path')
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
   cors: {
